f8badc4b57e9e2f4380f45d501c4379379d58e38
Per Andersen <per.j.andersen@gmail.com>
2011-04-03 09:01:08 +0000

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kistalk::Application.config.secret_token = '158f09c34057b804945ff4f7d9d7de66ba4b0280da118db7fdb2f28ce22a8ef5cdf2ec4fe6d3c30e468e60902c5cbe2bbbf58b9b4ecb350e42ca0d0cc3c6bc98'
